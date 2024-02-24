e63e984f9f841e51bdf3801b173e3f98f8148cd7
HHZ <860601963@qq.com>
2024-02-24 21:13:20 +0800

    feat(feature): add EventSearch

--- app/events/page.tsx
+++ app/events/page.tsx
@@ -1,12 +1,24 @@
+'use client';
+
+import { useRouter } from 'next/navigation';
+
 import { getAllEvents } from '@/dummy-data';
-import { EventList } from '@/components/events/EventList';
+import { EventList, EventSearch } from '@/components/events';
 
 export default function EventsPage() {
   const allEvents = getAllEvents();
+  const router = useRouter();
+
+  function findEventsHandler(year: string, month: string) {
+    const fullPath = `/events/${year}/${month}`;
+
+    router.push(fullPath);
+  }
 
   return (
-    <div>
+    <>
+      <EventSearch onSearch={findEventsHandler} />
       <EventList items={allEvents} />
-    </div>
+    </>
   );
 }
--- components/ui/Button.tsx
+++ components/ui/Button.tsx
@@ -1,13 +1,27 @@
+import { ReactEventHandler } from 'react';
 import Link from 'next/link';
 import styles from './Button.module.css';
 
-type ButtonProps = {
+type NormalButtonProps = {
+  onClick?: ReactEventHandler;
+  children: React.ReactNode;
+};
+
+type LinkButtonProps = {
   link: string;
   children: React.ReactNode;
 };
 
-function Button(props: ButtonProps) {
-  const { link } = props;
+function Button(props: NormalButtonProps | LinkButtonProps) {
+  const { link, onClick } = props as NormalButtonProps & LinkButtonProps;
+
+  if (!link) {
+    return (
+      <button onClick={onClick} className={styles.btn}>
+        {props.children}
+      </button>
+    );
+  }
 
   return (
     <Link href={link} className={styles.btn}>
